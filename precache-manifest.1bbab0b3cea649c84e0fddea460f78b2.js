aef2ead2588abe8da53d7f69b61c726e38bed470
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-05-08 17:53:17 -0500

    Updates

---
+++ precache-manifest.1bbab0b3cea649c84e0fddea460f78b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c19ba7f8824dd9b98d7df252c2ab7c5",
+    "url": "/new-movies/static/media/logo.2c19ba7f.svg"
+  },
+  {
+    "revision": "4b86e9327f36aa658a3502cf77762aed",
+    "url": "/new-movies/static/media/bg.4b86e932.jpg"
+  },
+  {
+    "revision": "0ab460be2e6e047dd20a",
+    "url": "/new-movies/static/js/runtime~main.b67c5510.js"
+  },
+  {
+    "revision": "87111b16ddf2c3d5878a",
+    "url": "/new-movies/static/js/main.7933192e.chunk.js"
+  },
+  {
+    "revision": "c2b5c3aff125dd21941f",
+    "url": "/new-movies/static/js/2.4864ef49.chunk.js"
+  },
+  {
+    "revision": "87111b16ddf2c3d5878a",
+    "url": "/new-movies/static/css/main.9f8187f4.chunk.css"
+  },
+  {
+    "revision": "099904878f624e2b0fff42cd59a0f0f2",
+    "url": "/new-movies/index.html"
+  }
+];
